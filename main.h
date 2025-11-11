a38f0aadfcf7e895c19eb9a168388fae539bcadb
shathasultan <shathasultann9@gmail.com>
2025-11-11 20:16:55 +0000

    main.h

--- main.h
+++ main.h
@@ -1,37 +1,26 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define MAX_ARGS 1024
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
+#include <errno.h>
+#include <limits.h>
 
 extern char **environ;
 
-/**
- * struct shell_info - Shell information
- * @name: Shell name
- * @cmd_count: Command counter
- * @last_status: Last command exit status
- */
-typedef struct shell_info
-{
-	char *name;
-	int cmd_count;
-	int last_status;
-} t_shell;
+char **get_argument(char *line);
+char *get_path(char *line);
+void free_args(char **array_command);
+void execute_command(char **array_command, int nbr_command);
+char *_getenv(const char *name);
 
-char *trim_spaces(char *str);
-int parse_args(char *line, char **args);
-char *_getenv(char *name);
-char *find_in_path(char *cmd);
-int handle_exit(char **args, char *line, char *copy, int last_status);
-int handle_env(void);
-int is_builtin(char **args, char *line, char *copy, int last_status);
-int exec_cmd(char **args, char *line, char *copy, t_shell *info);
-int process_line(char *line, t_shell *info);
+int main(int argc, char *argv[]);
 
 #endif
